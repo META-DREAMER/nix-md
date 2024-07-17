5e352cbb80df2e5bb7f100e0ae4a28f824ed8e3b
META DREAMER <jutt@ualberta.ca>
2024-07-17 00:43:18 -0600

    fix efi location

--- ghazali/configuration.nix
+++ ghazali/configuration.nix
@@ -15,7 +15,7 @@
   # Use the systemd-boot EFI boot loader.
   boot.loader.systemd-boot.enable = true;
   boot.loader.efi.canTouchEfiVariables = true;
-  boot.loader.efi.efiSysMountPoint = "/boot/efi";
+  boot.loader.efi.efiSysMountPoint = "/boot";
 
   services.openssh.enable = true;
 
