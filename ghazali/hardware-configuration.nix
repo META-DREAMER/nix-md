74af244a9ce92444dc179fcc436b91cf0acbe8cc
META DREAMER <jutt@ualberta.ca>
2024-07-17 02:06:28 -0600

    fix hardware config

--- ghazali/hardware-configuration.nix
+++ ghazali/hardware-configuration.nix
@@ -13,25 +13,6 @@
   boot.kernelModules = [ "kvm-intel" ];
   boot.extraModulePackages = [ ];
 
-  fileSystems."/" =
-    { device = "/dev/disk/by-uuid/fcc1891f-295d-4eb0-a918-92d723ebde5f";
-      fsType = "ext4";
-    };
-
-  fileSystems."/nix/store" =
-    { device = "/nix/store";
-      fsType = "none";
-      options = [ "bind" ];
-    };
-
-  fileSystems."/boot" =
-    { device = "/dev/disk/by-uuid/8220-F42C";
-      fsType = "vfat";
-      options = [ "fmask=0022" "dmask=0022" ];
-    };
-
-  swapDevices = [ ];
-
   # Enables DHCP on each ethernet and wireless interface. In case of scripted networking
   # (the default) this is the recommended approach. When using systemd-networkd it's
   # still possible to use this option, but it's recommended to use it in conjunction
