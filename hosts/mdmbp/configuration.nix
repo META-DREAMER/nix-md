d6371c3de1f1847eb3aff3082c626be3be6dc8e6
META DREAMER <jutt@ualberta.ca>
2024-12-09 15:34:12 -0700

    use username in configuration.nix

--- hosts/mdmbp/configuration.nix
+++ hosts/mdmbp/configuration.nix
@@ -8,10 +8,10 @@
   # The platform the configuration will be used on.
   nixpkgs.hostPlatform = "aarch64-darwin";
 
-  home-manager.users.hammadjutt = { ... }: {
+  home-manager.users.${username} = { ... }: {
     # Add this line to automatically backup existing files
     home.activation.checkLinkTargets.backupFileExtension = "backup";
     
     # ... rest of your home-manager configuration ...
   };
-} 
+}
