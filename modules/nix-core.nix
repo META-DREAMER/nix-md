6d91b6a1698cf8e39876b57687cc28fd393de5bd
META DREAMER <jutt@ualberta.ca>
2024-12-09 15:11:27 -0700

    update substituters

--- modules/nix-core.nix
+++ modules/nix-core.nix
@@ -1,4 +1,3 @@
-
 { pkgs, ... }:
 
 {
@@ -8,11 +7,14 @@
 
     # substituers that will be considered before the official ones(https://cache.nixos.org)
     substituters = [
-      "https://mirror.sjtu.edu.cn/nix-channels/store"
-      "https://nix-community.cachix.org"
+      "https://cache.nixos.org"                     # Official cache
+      "https://nix-community.cachix.org"            # Popular community cache
+      "https://cache.garnix.io"                     # Fast mirror, good for flakes
     ];
     trusted-public-keys = [
-      "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
+      "cache.nixos.org-1:6NCHdD59X431o0gWypbMrAURkbJ16ZPMQFGspcDShjY="  # Official cache key
+      "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="  # Community cache key
+      "cache.garnix.io:CTFPyKSLcx5RMJKfLo5EEPUObbA78b0YQ2DTCJXqr9g="            # Garnix cache key
     ];
     builders-use-substitutes = true;
   };
